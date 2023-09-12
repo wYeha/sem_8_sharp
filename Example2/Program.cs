b1bbd74d366be1cbaed3a2f68379b860b3722dc7
Wyeha <snovavzloman@mail.ru>
2023-09-12 19:42:49 +0300

    1st commit

---
+++ Example2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Random rnd = new Random();
+
+int[,] FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SmallestStringSumm(int[,] matrix){
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+    
+    }
+}
