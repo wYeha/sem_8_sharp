6320523aacc280363fbb803f5faeaf5b4956c1e5
Wyeha <snovavzloman@mail.ru>
2023-09-12 22:11:15 +0300

    5th done

--- Example5/Program.cs
+++ Example5/Program.cs
@@ -1,48 +1,43 @@
 ﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
 
-int[,] FillMatrix(int[,] matrix)
+int n = 4; 
+int[,] array = new int[n, n];
+
+int value = 1; 
+int minRow = 0, maxRow = n - 1, minCol = 0, maxCol = n - 1;
+
+while (value <= n * n)
 {
-    int flag = 4, i = 0, j = 0, count = 1;
-    while (flag > 0)
+    for (int i = minCol; i <= maxCol; i++)
     {
-        for (j; j < flag; j++)
-        {
-            matrix[i, j] = count;
-            count++;
-        }
-        for (i; i < flag; i++)
-        {
-            matrix[i, j] = count;
-            count++;
-        }
-        for (j; j > 4 - flag; j--)
-        {
-            matrix[i, j] = count;
-            count++;
-        }
-        flag--;
-        for (i; i > 4 - flag; i--)
-        {
-            matrix[i, j] = count;
-            count++;
-        }
+        array[minRow, i] = value++;
     }
-    return matrix;
+    minRow++;
+
+    for (int i = minRow; i <= maxRow; i++)
+    {
+        array[i, maxCol] = value++;
+    }
+    maxCol--;
+
+    for (int i = maxCol; i >= minCol; i--)
+    {
+        array[maxRow, i] = value++;
+    }
+    maxRow--;
+
+    for (int i = maxRow; i >= minRow; i--)
+    {
+        array[i, minCol] = value++;
+    }
+    minCol++;
 }
 
-void PrintMatrix(int[,] matrix)
+for (int i = 0; i < n; i++)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int j = 0; j < n; j++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j],3} ");
-        }
-
-        Console.WriteLine();
+        Console.Write($"{array[i, j], 3} ");
     }
+    Console.WriteLine();
 }
-
-
-int[,] array = new int[4, 4];
-PrintMatrix(FillMatrix(array));
