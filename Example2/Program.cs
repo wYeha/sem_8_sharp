898c7bf07196a162fdf69ac57a5224166961ec18
Wyeha <snovavzloman@mail.ru>
2023-09-12 20:39:19 +0300

    2nd done

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -8,9 +8,10 @@ int[,] FillMatrix(int[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(1, 10);
+            matrix[i, j] = rnd.Next(-20, 20);
         }
     }
+    
     return matrix;
 }
 
@@ -22,13 +23,33 @@ void PrintMatrix(int[,] matrix)
         {
             Console.Write($"{matrix[i, j],3} ");
         }
+
         Console.WriteLine();
     }
 }
 
-int SmallestStringSumm(int[,] matrix){
+int SmallestStringSumm(int[,] matrix)
+{
+    int minSum = int.MaxValue;
+    int minSumRow = 0;
+
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-    
+        int sum = 0;
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            minSumRow = i;
+        }
     }
+    return minSumRow;
 }
+
+int[,] array = new int[5, 4];
+PrintMatrix(FillMatrix(array));
+Console.WriteLine($"Строка с наименьшей суммой элементов под индексом {SmallestStringSumm(array)}");
